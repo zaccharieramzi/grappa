a0cbacbc7d0cead2b04a9863ad476725a65b65c2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-08-17 18:46:19 +0200

    added multicoil double geometry estimation test

--- grappa/tests/kernel_estimation_test.py
+++ grappa/tests/kernel_estimation_test.py
@@ -113,39 +113,39 @@ def test_geometry_kernel_estimation_singlecoil_double(ac, ny, i_geom, expected_k
         1,
         [[1, 1, 0, 0], [-1, -1, 0, 0]],
     ),
-        (  # second example, a bit more complex, still in single geometry
+    (  # second example, a bit more complex, still in single geometry
+        [
             [
-                [
-                    [1, 2, 1],
-                    [2, 4, 2],
-                    [3, 6, 3],
-                    [7, 2, -5],
-                    [0.1, 1.1, 1],
-                    [3.1, 5.1, 2],
-                    [3.1, 12.1, 9],
-                    [7.1, 3.1, -4],
-                    [8, 3.5, -4.5],
-                    [18, 12, -6],
-                ],
-                [
-                    [0.001, -2, 0],
-                    [0.002, -4, 0],
-                    [0.0001, -6, 0],
-                    [-0.001, -2, 0],
-                    [0.0003, -1.1, 0],
-                    [0.0005, -5.1, 0],
-                    [-0.002, -12.1, 0],
-                    [0.0007, -3.1, 0],
-                    [0.0002, -3.5, 0],
-                    [0.0004, -12, 0],
-                ],
+                [1, 2, 1],
+                [2, 4, 2],
+                [3, 6, 3],
+                [7, 2, -5],
+                [0.1, 1.1, 1],
+                [3.1, 5.1, 2],
+                [3.1, 12.1, 9],
+                [7.1, 3.1, -4],
+                [8, 3.5, -4.5],
+                [18, 12, -6],
             ],
-            3,
             [
-                [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0.001, -2, 0],
+                [0.002, -4, 0],
+                [0.0001, -6, 0],
+                [-0.001, -2, 0],
+                [0.0003, -1.1, 0],
+                [0.0005, -5.1, 0],
+                [-0.002, -12.1, 0],
+                [0.0007, -3.1, 0],
+                [0.0002, -3.5, 0],
+                [0.0004, -12, 0],
             ],
-        )
+        ],
+        3,
+        [
+            [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0],
+        ],
+    )
 ])
 def test_geometry_kernel_estimation_multicoil_simple(ac, ny, expected_kernel):
     ac = np.array(ac)
@@ -155,6 +155,41 @@ def test_geometry_kernel_estimation_multicoil_simple(ac, ny, expected_kernel):
         i_geom=0,
         ny=ny,
         n_geometries=1,
+        ncoils=2,
+    )
+    np.testing.assert_array_almost_equal(expected_kernel, grappa_kernel, decimal=3)
+
+
+@pytest.mark.parametrize('ac, ny, i_geom, expected_kernel', [
+    (  # first basic example with a kernel using only the 2 direct neighbours
+       # from the first coil (either positively or negatively)
+        [
+            [
+                [1, 2, 1, 1],
+                [2, 4, 2, 2],
+                [3, 6, 3, 3],
+                [7, 2, -5, -5],
+            ],
+            [
+                [0.001, -2, 0, 0],
+                [0.004, -4, 0, 0],
+                [-0.001, -6, 0, 0],
+                [0.002, -2, 0, 0],
+            ],
+        ],
+        1,
+        0,
+        [[1, 1, 0, 0], [-1, -1, 0, 0]],
+    ),
+])
+def test_geometry_kernel_estimation_multicoil_double(ac, ny, i_geom, expected_kernel):
+    ac = np.array(ac)
+    expected_kernel = np.array(expected_kernel)
+    grappa_kernel = _geometry_kernel_estimation(
+        ac,
+        i_geom=i_geom,
+        ny=ny,
+        n_geometries=2,
         ncoils=2,
     )
     np.testing.assert_array_almost_equal(expected_kernel, grappa_kernel, decimal=3)
