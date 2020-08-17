add095ff6d76f60c64ec908736c95b96e8ae23e5
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-08-17 18:07:04 +0200

    refactored kernel estimation into a more pytest-y fashion

--- grappa/tests/kernel_estimation_test.py
+++ grappa/tests/kernel_estimation_test.py
@@ -1,16 +1,23 @@
 import numpy as np
+import pytest
 
 from grappa.kernel_estimation import _geometry_kernel_estimation
 
 
-def test_geometry_kernel_estimation():
-    ac = np.array([
-        [1, 2, 1],
-        [2, 4, 2],
-        [3, 6, 3],
-        [7, 2, -5],
-    ])[None, :]
-    expected_kernel = np.array([[1]*2])
+@pytest.mark.parametrize('ac, expected_kernel',[
+    (
+        [
+            [1, 2, 1],
+            [2, 4, 2],
+            [3, 6, 3],
+            [7, 2, -5],
+        ],
+        [[1]*2],
+    )
+])
+def test_geometry_kernel_estimation(ac, expected_kernel):
+    ac = np.array(ac)[None, :]
+    expected_kernel = np.array(expected_kernel)
     grappa_kernel = _geometry_kernel_estimation(
         ac,
         i_geom=0,
