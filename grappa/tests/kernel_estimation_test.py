2a4af722cd126dc96ea09e42990822b158c5c023
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-08-17 18:33:55 +0200

    added kernel estimation multicoil test

--- grappa/tests/kernel_estimation_test.py
+++ grappa/tests/kernel_estimation_test.py
@@ -92,3 +92,36 @@ def test_geometry_kernel_estimation_singlecoil_double(ac, ny, i_geom, expected_k
         ncoils=1,
     )
     np.testing.assert_array_almost_equal(expected_kernel, grappa_kernel)
+
+@pytest.mark.parametrize('ac, ny, expected_kernel', [
+    (  # first basic example with a kernel using only the 2 direct neighbours
+       # from the first coil (either positively or negatively)
+        [
+            [
+                [1, 2, 1],
+                [2, 4, 2],
+                [3, 6, 3],
+                [7, 2, -5],
+            ],
+            [
+                [0.001, -2, 0],
+                [0.004, -4, 0],
+                [-0.001, -6, 0],
+                [0.002, -2, 0],
+            ],
+        ],
+        1,
+        [[1, 1, 0, 0], [-1, -1, 0, 0]],
+    ),
+])
+def test_geometry_kernel_estimation_multicoil_simple(ac, ny, expected_kernel):
+    ac = np.array(ac)
+    expected_kernel = np.array(expected_kernel)
+    grappa_kernel = _geometry_kernel_estimation(
+        ac,
+        i_geom=0,
+        ny=ny,
+        n_geometries=1,
+        ncoils=2,
+    )
+    np.testing.assert_array_almost_equal(expected_kernel, grappa_kernel)
