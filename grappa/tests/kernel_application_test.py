413bfc79c5f51ae442086d402b639729cc29fdd7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-08-17 19:28:19 +0200

    added another kernel application test

--- grappa/tests/kernel_application_test.py
+++ grappa/tests/kernel_application_test.py
@@ -1,29 +1,48 @@
 import numpy as np
+import pytest
 
 from grappa.kernel_application import _geom_apply_kernel
 
 
-def test_geom_apply_kernel():
-    kspace = [
-        [1, 0, 4],
-        [2, 0, 5],
-        [3, 0, 6],
-    ]
-    expected_kspace = [
-        [1, 5, 4],
-        [2, 7, 5],
-        [3, 9, 6],
-    ]
-    grappa_kernel = [[1]*2]
+@pytest.mark.parametrize('kspace, grappa_kernel, expected_kspace',[
+    (
+        [
+            [1, 0, 4],
+            [2, 0, 5],
+            [3, 0, 6],
+        ],
+        [[1]*2],
+        [
+            [1, 5, 4],
+            [2, 7, 5],
+            [3, 9, 6],
+        ],
+    ),
+    (
+        [
+            [1, 0, 4],
+            [2, 0, 5],
+            [3, 0, 6],
+        ],
+        [[1]*6],
+        [
+            [1, 0, 4],
+            [2, 21, 5],
+            [3, 0, 6],
+        ],
+    )
+])
+def test_geom_apply_kernel(kspace, grappa_kernel, expected_kspace):
     kspace = np.array(kspace)[None, :]
     expected_kspace = np.array(expected_kspace)[None, :]
     grappa_kernel = np.array(grappa_kernel)
+    ny = grappa_kernel.size // 2
     _geom_apply_kernel(
         kspace,
         grappa_kernel,
         0,
         1,
-        1,
+        ny,
         1,
     )
     np.testing.assert_array_almost_equal(expected_kspace, kspace)
