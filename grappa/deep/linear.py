95d61f4286b78836e168d623c2ab9d0ddeab38a5
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-06 10:29:16 +0100

    added an unused arg for linear grappa to match current api

--- grappa/deep/linear.py
+++ grappa/deep/linear.py
@@ -2,6 +2,6 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tf_complex.dense import ComplexDense
 
-def linear_deep_grappa_model(ncoils=15):
+def linear_deep_grappa_model(ncoils=15, _distance_from_center_feat=False):
     model = Sequential([ComplexDense(ncoils, use_bias=False, activation='linear', dtype=tf.complex64)])
     return model
