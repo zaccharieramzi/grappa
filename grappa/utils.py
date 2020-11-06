24ddf05ef5a7a11d3d7a00af31ac1cd38751e760
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-06 20:11:43 +0100

    added a unit tested pinv working for complex tensors

--- grappa/utils.py
+++ grappa/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import tensorflow as tf
+
 
 def cartesian_product(*arrays):
     # taken from https://stackoverflow.com/a/11146645/4332585
@@ -39,3 +41,55 @@ def number_geometries(mask):
     second_1 = sampled_lines[1]
     n_geometries = second_1 - first_1 - 1
     return n_geometries
+
+def pinv(a, rcond=None):
+    """Taken from
+    https://github.com/tensorflow/tensorflow/blob/v2.3.1/tensorflow/python/ops/linalg/linalg_impl.py
+    """
+    dtype = a.dtype.as_numpy_dtype
+
+    if rcond is None:
+        def get_dim_size(dim):
+            dim_val = a.shape[dim]
+            if dim_val is not None:
+                return dim_val
+            return tf.shape(a)[dim]
+
+        num_rows = get_dim_size(-2)
+        num_cols = get_dim_size(-1)
+        if isinstance(num_rows, int) and isinstance(num_cols, int):
+            max_rows_cols = float(max(num_rows, num_cols))
+        else:
+            max_rows_cols = tf.cast(tf.maximum(num_rows, num_cols), dtype)
+        rcond = 10. * max_rows_cols * np.finfo(dtype).eps
+
+    rcond = tf.convert_to_tensor(rcond, dtype=dtype, name='rcond')
+
+    # Calculate pseudo inverse via SVD.
+    # Note: if a is Hermitian then u == v. (We might observe additional
+    # performance by explicitly setting `v = u` in such cases.)
+    [
+        singular_values,  # Sigma
+        left_singular_vectors,  # U
+        right_singular_vectors,  # V
+    ] = tf.linalg.svd(
+        a, full_matrices=False, compute_uv=True)
+
+    # Saturate small singular values to inf. This has the effect of make
+    # `1. / s = 0.` while not resulting in `NaN` gradients.
+    cutoff = tf.cast(rcond, dtype=singular_values.dtype) * tf.reduce_max(singular_values, axis=-1)
+    singular_values = tf.where(
+        singular_values > cutoff[..., None], singular_values,
+        np.array(np.inf, dtype))
+
+    # By the definition of the SVD, `a == u @ s @ v^H`, and the pseudo-inverse
+    # is defined as `pinv(a) == v @ inv(s) @ u^H`.
+    a_pinv = tf.matmul(
+        right_singular_vectors / tf.cast(singular_values[..., None, :], dtype=dtype),
+        left_singular_vectors,
+        adjoint_b=True)
+
+    if a.shape is not None and a.shape.rank is not None:
+      a_pinv.set_shape(a.shape[:-2].concatenate([a.shape[-1], a.shape[-2]]))
+
+    return a_pinv
