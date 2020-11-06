eeb2b837c2ace3a712b9d0133d6e6b38ff65e917
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-06 19:12:18 +0100

    corrected dims in benchmark and print

--- benchmark.py
+++ benchmark.py
@@ -7,16 +7,17 @@ from grappa.reconstruction import slice_reconstruction
 
 # Benchmark params
 R = 4
-dim = 256
+readout_dim = 512
+phase_dim = 256
 ncoils = 15
 num_grappa = 20
 
 # Data generation
-kspace = np.random.normal(size=[ncoils, dim, dim]).astype(np.complex64)
+kspace = np.random.normal(size=[ncoils, readout_dim, phase_dim]).astype(np.complex64)
 mask = np.array([
-    i%R == 0 for i in range(dim)
+    i%R == 0 for i in range(phase_dim)
 ])
-mask[dim//2-12:dim//2+12] = True
+mask[phase_dim//2-12:phase_dim//2+12] = True
 kspace_undersampled = kspace * mask[None, None, :].astype(np.complex64)
 
 for backend in ['numpy', 'tensorflow']:
@@ -42,4 +43,4 @@ for backend in ['numpy', 'tensorflow']:
         )
     end_time = time.perf_counter()
     avg_time = (end_time-start_time) / num_grappa
-    print('{backend} backend takes {avg_time}s')
+    print(f'{backend} backend takes {avg_time}s')
